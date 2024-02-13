a0c43348401953979cf4551cb703bc569d1098df
leventkalkavan <levent.kalkavan@hotmail.com>
2024-02-13 19:51:20 +0300

    third part - Exchange types / Topic Exchange

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -5,30 +5,23 @@ var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://umlnivor:onJcuoGCaUsIzS4T52YRgGBC1HfIoU3y@fish.rmq.cloudamqp.com/umlnivor");
 
 using var connection = factory.CreateConnection();
+
 var channel = connection.CreateModel();
 
-channel.ExchangeDeclare("logs-directs", durable: true, type: ExchangeType.Direct);
+channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
 
-Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
-{
-    var routeKey = $"route-{x}";
-    var queueName = $"direct-queue-{x}";
-    channel.QueueDeclare(queueName, true, false, false);
 
-    channel.QueueBind(queueName, "logs-directs", routeKey, null);
-});
-
+Random rnd = new Random();
 Enumerable.Range(1, 50).ToList().ForEach(x =>
 {
-    LogNames log = (LogNames)new Random().Next(1, 5);
+    LogNames log1 = (LogNames)rnd.Next(1, 5);
+    LogNames log2 = (LogNames)rnd.Next(1, 5);
+    LogNames log3 = (LogNames)rnd.Next(1, 5);
 
-    string message = $"log-type: {log}";
-
+    var routeKey = $"{log1}.{log2}.{log3}";
+    string message = $"log-type: {log1}-{log2}-{log3}";
     var messageBody = Encoding.UTF8.GetBytes(message);
-
-    var routeKey = $"route-{log}";
-
-    channel.BasicPublish("logs-directs", routeKey, null, messageBody);
+    channel.BasicPublish("logs-topic", routeKey, null, messageBody);
 
     Console.WriteLine($"Log gönderilmiştir : {message}");
 });
