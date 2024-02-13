acaec4de1e1ffb2b1ef14687ed06952bc188a7ef
leventkalkavan <levent.kalkavan@hotmail.com>
2024-02-13 14:53:46 +0300

    second part - hello world

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://umlnivor:onJcuoGCaUsIzS4T52YRgGBC1HfIoU3y@fish.rmq.cloudamqp.com/umlnivor");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("helloque", true, false, false);
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message {x}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "helloque", null, messageBody);
+    Console.WriteLine($"mesaj gonderildi: {message}");
+});
+Console.ReadLine();
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://umlnivor:onJcuoGCaUsIzS4T52YRgGBC1HfIoU3y@fish.rmq.cloudamqp.com/umlnivor");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+//publisherda olustugundan eminsen channeli silebilirsin ama degilsen burda otomatik olusacaktir hata almazsin
+//channel.QueueDeclare("helloque", true, false, false);
+
+channel.BasicQos(0,1,true);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("helloque", false, consumer);
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("gelen mesaj: " + message);
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+Console.ReadLine();
