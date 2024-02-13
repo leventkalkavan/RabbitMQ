079bff6b1ec9ca0846f6b8a579f3bfb2337ff9a6
leventkalkavan <levent.kalkavan@hotmail.com>
2024-02-13 18:17:51 +0300

    third part - Exchange types / Direct Exchange

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -6,12 +6,39 @@ factory.Uri = new Uri("amqps://umlnivor:onJcuoGCaUsIzS4T52YRgGBC1HfIoU3y@fish.rm
 
 using var connection = factory.CreateConnection();
 var channel = connection.CreateModel();
-channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+channel.ExchangeDeclare("logs-directs", durable: true, type: ExchangeType.Direct);
+
+Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+{
+    var routeKey = $"route-{x}";
+    var queueName = $"direct-queue-{x}";
+    channel.QueueDeclare(queueName, true, false, false);
+
+    channel.QueueBind(queueName, "logs-directs", routeKey, null);
+});
+
 Enumerable.Range(1, 50).ToList().ForEach(x =>
 {
-    string message = $"Message {x}";
+    LogNames log = (LogNames)new Random().Next(1, 5);
+
+    string message = $"log-type: {log}";
+
     var messageBody = Encoding.UTF8.GetBytes(message);
-    channel.BasicPublish("logs-fanout", "", null, messageBody);
-    Console.WriteLine($"mesaj gonderildi: {message}");
+
+    var routeKey = $"route-{log}";
+
+    channel.BasicPublish("logs-directs", routeKey, null, messageBody);
+
+    Console.WriteLine($"Log gönderilmiştir : {message}");
 });
+
 Console.ReadLine();
+
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -6,22 +6,27 @@ var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://umlnivor:onJcuoGCaUsIzS4T52YRgGBC1HfIoU3y@fish.rmq.cloudamqp.com/umlnivor");
 
 using var connection = factory.CreateConnection();
+
 var channel = connection.CreateModel();
 
-//kalici kuyruk icin
-var randemQue = "log-database-save-queque"; 
-    //channel.QueueDeclare().QueueName; gecici kuyruk
-channel.QueueDeclare(randemQue, true, false, false);
-channel.QueueBind(randemQue, "logs-fanout", "", null);
 
-channel.BasicQos(0, 1, true);
+channel.BasicQos(0, 1, false);
 var consumer = new EventingBasicConsumer(channel);
-channel.BasicConsume(randemQue, false, consumer);
-Console.WriteLine("Loglar dinleniyor..");
+
+var queueName = "direct-queue-Critical";
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Logları dinleniyor...");
+
 consumer.Received += (object sender, BasicDeliverEventArgs e) =>
 {
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
-    Console.WriteLine("gelen mesaj: " + message);
+
+    Thread.Sleep(1500);
+    Console.WriteLine("Gelen Mesaj:" + message);
+
+    // File.AppendAllText("log-critical.txt", message+ "\n");
+
     channel.BasicAck(e.DeliveryTag, false);
 };
 
